d32af5a184dcd4a5b597cb92698ed4acdd60bc6f
Damir D <damir180501@mail.ru>
2021-02-22 23:06:50 +0300

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 20;
+    public float turnRate = 10;
+    private float inputVer = 0;
+    private float inputHor = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVer = Input.GetAxis("Vertical");
+        inputHor = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * inputVer);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnRate * inputHor);
+    }
+}
